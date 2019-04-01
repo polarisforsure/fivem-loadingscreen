3790de630dbffcd98af732738a0560ee4f972457
polarisforsure <43050319+polarisforsure@users.noreply.github.com>
2019-04-01 13:48:33 +0200

    Add files via upload

---
+++ loadingscreen/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 5000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "aTYdSQrBA0o",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 70,
+    "text": {
+        "title": "Exodus RP Discord",
+        "link": "https://discord.gg/HxDJ8xn",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png']
+}
